488355c59ae9821bc39edf8ecea7ed0d8b54384e
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-06-03 02:04:59 +0200

    feat: add functions.php and configuration file

--- wp-content/themes/client/functions.php
+++ wp-content/themes/client/functions.php
@@ -1,19 +1,12 @@
 <?php
-// Démarrer le système de sessions pour pouvoir afficher des messages de feedback
-if (session_status() === PHP_SESSION_NONE) {
-  session_start();
-}
-
-// Charger les fichiers des fonctionnalités extraites dans des classes.
-require_once(__DIR__ . '/core/controllers/ContactForm.php');
-
 include('core/theme/configuration.php');
 
-register_nav_menu('header', 'Le menu qui se trouve dans le header');
+register_nav_menu('header', 'Le menu qui se trouve dans le header parents');
 register_nav_menu('footer', 'Le menu qui se trouve dans le footer');
-register_nav_menu('social-media', 'Le menu qui regroupe nos réseaux sociaux');
+register_nav_menu('header-connecte', 'Le menu pour les enseignants connectés');
+//register_nav_menu('social-media', 'Le menu qui regroupe nos réseaux sociaux');
 
-function dw_get_navigation_links(string $menu_name): array
+function portfolio_get_navigation_links(string $menu_name): array
 {
   // Récupérer l'objet WP pour le menu à la location $location
   $all_menus = get_nav_menu_locations();
@@ -40,7 +33,7 @@ function dw_get_navigation_links(string $menu_name): array
   return $links;
 }
 
-dw_get_navigation_links('header');
+portfolio_get_navigation_links('header');
 
 function dw_asset(string $filename): string
 {
@@ -49,113 +42,16 @@ function dw_asset(string $filename): string
   if (file_exists($manifest_path)) {
     $manifest = json_decode(file_get_contents($manifest_path), true);
 
-    if (isset($manifest['wp-content/themes/dw/assets/css/styles.scss']) && $filename === 'css') {
-      return get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/assets/css/styles.scss']['file']);
+    if (isset($manifest['wp-content/themes/client/assets/css/styles.scss']) && $filename === 'css') {
+      return get_theme_file_uri('public/' . $manifest['wp-content/themes/client/assets/css/styles.scss']['file']);
     }
 
-    if (isset($manifest['wp-content/themes/dw/assets/js/main.js']) && $filename === 'js') {
-      return get_theme_file_uri('public/' . $manifest['wp-content/themes/dw/assets/js/main.js']['file']);
+    if (isset($manifest['wp-content/themes/client/assets/js/main.js']) && $filename === 'js') {
+      return get_theme_file_uri('public/' . $manifest['wp-content/themes/client/assets/js/main.js']['file']);
     }
   }
 
   return '';
-}
-
-register_post_type('formation', [
-  'label' => 'Formations',
-  'description' => 'Les formations présentes sur mon site web',
-  'menu_position' => 2,
-  'menu_icon' => 'dashicons-welcome-learn-more',
-  'public' => true,
-  'has_archive' => true,
-  'supports' => ['title', 'excerpt', 'thumbnail'],
-  'rewrite' => [
-    'slug' => 'formations'
-  ],
-]);
-
-register_post_type('contact_message', [
-  'label' => 'Mes messages de contact',
-  'description' => 'Mes messages de contact',
-  'menu_position' => 3,
-  'menu_icon' => 'dashicons-welcome-learn-more',
-  'public' => false,
-  'show_ui' => true,
-  'has_archive' => false,
-  'supports' => ['title', 'editor'],
-]);
-
-register_taxonomy('difficulty', 'formation', [
-  'hierarchical' => true,
-  'labels' => [
-    'name' => 'Le level des formations'
-  ],
-  'show_ui' => true,
-  'show_admin_column' => true,
-  'query_var' => true,
-]);
-
-add_action('admin_post_nopriv_hepl_contact_form', 'hepl_execute_contact_form');
-add_action('admin_post_hepl_contact_form', 'hepl_execute_contact_form');
-
-function hepl_execute_contact_form(): void
-{
-  $config = [
-    'nonce_field' => 'contact_nonce',
-    'nonce_identifier' => 'hepl_contact_form'
-  ];
-
-  (new ContactForm($config, $_POST))
-    ->sanitize([ // nettoyer les données
-      'name' => 'text_field', // Je lancerai sanitize_text_field()
-      'email' => 'email',  // Je lancerai sanitize_email()
-      'object' => 'text_field',  // Je lancerai sanitize_text_field()
-      'message' => 'textarea_field',  // Je lancerai sanitize_textarea_field()
-    ])
-    ->validate([ // valider les données
-      'name' => ['required'],
-      // Je définis la règle de validation required que j'exécuterai plus tard
-      'email' => ['email', 'required'],
-      // Je définis les règles de validation required et email que j'exécuterai plus tard
-      'object' => [],
-      //
-      'message' => ['required'],
-      // Je définis la règle de validation required que j'exécuterai plus tard
-    ])
-    ->save( // sauvegarder les données dans un CPT -> message de contact
-      title: fn($data) => $data['name'] . ' - ' . $data['email'] . ' - ' . $data['object'],
-      // John Doe - johndoe@example.com - Mon objet
-      content: fn($data) => $data['message'],
-    )
-    ->send( // J'envoie les données à mon administrateur du site
-      title: fn($data) => 'Nouveau message de ' . $data['name'], // John Doe - johndoe@example.com - Mon objet
-      content: fn($data
-      ) => 'Nom complet: ' . $data['name'] . PHP_EOL . 'Adresse mail: ' . $data['email'] . PHP_EOL . 'Objet: ' . $data['object'] . PHP_EOL . 'Message: ' . $data['message'],
-    )
-    ->feedback();
-}
-
-function hepl_session_flash($key, $value): void
-{
-  if (!isset($_SESSION['hepl_flash'])) {
-    $_SESSION['hepl_flash'] = [];
-  }
-
-  $_SESSION['hepl_flash'][$key] = $value;
-}
-
-function hepl_session_get(string $key)
-{
-  if (isset($_SESSION['hepl_flash']) && array_key_exists($key, $_SESSION['hepl_flash'])) {
-    // 1. Récupérer la donnée qui a été flash
-    $value = $_SESSION['hepl_flash'][$key];
-    // 2. Supprimer la donnée de la session
-    unset($_SESSION['hepl_flash'][$key]);
-    // 3. Retourner ma donnée récupérée
-    return $value;
-  }
-
-  return null;
 }
 
 //charger les traductions existantes
@@ -167,11 +63,29 @@ function __hepl(string $translation): ?string
   return __($translation, 'hepl-trad');
 }
 
+register_post_type('sensibilisation', [
+    'label' => 'Sensibilisations',
+    'description' => 'Les sensibilisations présentes sur mon site web',
+    'menu_position' => 2,
+    'menu_icon' => 'dashicons-welcome-view-site',
+    'public' => true,
+    'has_archive' => false,
+    'supports' => ['title'],
+    'rewrite' => [
+        'slug' => 'sensibilisations'
+    ],
+]);
 
+function my_own_mime_types( $mimes ) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'my_own_mime_types' );
 
-
-
-
+// quand c'est a true WP recadre 'crop' mon img et quand c'est false il redimensionne proportionnellement sans me la couper
+add_image_size('square-small', 400, 400, true);
+add_image_size('square-medium', 800, 800, true);
+add_image_size('square-large', 1200, 1200, true);
 
 
 
