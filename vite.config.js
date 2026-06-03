47e486404861fa18878e91f6dbe92c6ba578d6aa
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-06-03 02:03:29 +0200

    chore: add Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,28 @@
+import {defineConfig} from "vite";
+import {globSync} from "glob";
+import fs from "fs";
+
+export default defineConfig({
+    base: '/wp-content/themes/client/public/',
+    plugins: [
+        {
+            name: 'bundle-js',
+        }
     ],
+
+
+    build: {
+        manifest: true,
+        rollupOptions: {
+            input: {
+                js: './wp-content/themes/client/assets/js/main.js',
+                scss: './wp-content/themes/client/assets/css/styles.scss',
+            },
+            output: {
+                dir: './wp-content/themes/client/public'
+            }
+        },
+        assetsInlineLimit: 0,
+        target: ["es2015"]
+    }
+})
