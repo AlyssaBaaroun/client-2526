53d9287cd526a97c7a720b920dc0d0a16b8346e0
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-06-03 02:03:08 +0200

    feat: add settings configuration file

---
+++ wp-content/themes/client/assets/js/settings.js
@@ -0,0 +1,13 @@
+export const settings = {
+    burgerBtn: '.js-menu-toggle-btn',
+    navList: '.navigation__list',
+    setTimeOut: 300,
+    burgerAnim: 'hamburger-menu--active',
+    checkIsOpen: 'navigation__list--open',
+    checkIsClosed: 'navigation__list--close',
+
+    /*ici, c'est pour mes animations*/
+    showUp: 'showup',
+    hidden: 'hidden',
+    toAnimate: '[data-showup="true"]',
+}
