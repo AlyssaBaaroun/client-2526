896986691e42c9e632ed70750e759a624a623f0e
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-06-14 05:08:12 +0200

    feat: add 404 error page layout and styling

---
+++ wp-content/themes/client/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="error">
+    <h2 class="error__title">404</h2>
+    <p class="error__text">Désolé, la page que vous recherchez n'existe pas ou a été déplacée.</p>
+    <a class="error__btn-acc" href="/" title="Vers la page d'accueil">Retour à la page d'accueil</a>
+</section>
+
+<?php get_footer(); ?>
